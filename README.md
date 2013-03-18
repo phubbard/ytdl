7e76872efbf51cc6139f5b0e1713518891f89db9
Paul Hubbard <paul.hubbard@verizon.com>
2013-03-18 15:38:11 -0700

    complete set of files

---
+++ README.md
@@ -0,0 +1,22 @@
+## What is this?
+
+I have a semi-technical mom. She wants a simple way to download youtube videos on her
+Macintosh. Running the awesome https://github.com/rg3/youtube-dl exceeds her skills,
+so I wrote a 3-liner shell script that
+
+- cds into ~/Movies
+- Takes the clipboard and feeds it to youtube-dl
+
+Usage is pretty easy - she copies the URL into the clipboard, then double-clicks 
+'download-youtube' on her desktop. The file is auto-named using the -l function of
+youtube-dl, and placed in her Movies directory. I also had to install VLC to play
+FLV files, but it was a decent solution.
+
+Then she wanted to be able to replicate this for her better half. Who isn't here on this
+visit. So I wrote this repo, which is a shell script to download the files, put them into
+~/bin and ~/Desktop, and set the permissions (755) accordingly.
+
+## How It Should Work (to be tested)
+
+curl -O https://github.com/phubbard/ytdl/raw/master/automatic.sh && ./automatic.sh
+
--- automatic.sh
+++ automatic.sh
@@ -8,6 +8,10 @@
 #  c) Put the 'download-youtube' script onto the desktop
 #
 # That's all!
+# Intended usage is a one-liner I can email family:
+# curl -O https://github.com/phubbard/ytdl/raw/master/automatic.sh; sh automatic.sh
+# 
+# Notes: youtube-dl in ~/bin, download-youtube in ~/Desktop, movies into ~/Movies
 
 echo -n "Checking for directory ~/bin..."
 
@@ -24,4 +28,38 @@ else
   echo "Unable to create or use ~/bin, exiting"
   exit 1
  fi
+fi
+
+cd $HOME/bin
+echo -n "Downloading youtube-dl..."
+curl -O https://github.com/rg3/youtube-dl/raw/master/youtube-dl
+if [ -f youtube-dl ]
+then
+  echo "downloaded."
+  echo -n "Making executable..."
+  chmod 755 youtube-dl
+  echo "done"
+else
+  echo "Unable to download youtube-dl, exiting" 
+  exit 2
+fi
+
+if [ -d $HOME/Desktop ]
+then
+ echo "Heading to Desktop to download wrapper script"
+ cd $HOME/Desktop
+ echo "Downloading wrapper..."
+ curl -O https://github.com/phubbard/ytdl/raw/master/download-youtube
+ if [ -f download-youtube ]
+  echo "downloaded, marking executable"
+  chmod 755 download-youtube
+  echo "And we're all done! Enjoy."
+  exit 0
+ else
+  echo "Error downloading wrapper, maybe check and retry"
+  exit 3
+ fi
+else
+ echo "Unable to find destination Desktop directory"
+ exit 4
 fi
